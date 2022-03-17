c378212e5bf7433ccfc219748434733e2822bbd5
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-03-17 18:39:47 +0800

    Update load.py

--- CIFAR/load.py
+++ CIFAR/load.py
@@ -4,7 +4,7 @@ sys.path.append('..')
 import numpy as np
 import os
 
-data_dir = '/home/wuiron/Joint/data/cifar'
+
 
 def unpickle(file):
     import pickle
@@ -12,7 +12,7 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-def cifar10():
+def cifar10(data_dir = '/home/wuiron/Joint/data/cifar'):
     for k in range(1,6):
         train = unpickle(data_dir+'/data_batch_{}'.format(k))
         train_raw = train[b'data']
@@ -27,7 +27,7 @@ def cifar10():
 
     return trX
 
-def cifar100():
+def cifar100(data_dir = '/home/wuiron/Joint/data/cifar'):
     train = unpickle(data_dir+'/train')
     train_raw = train[b'data']
     trX = train_raw.reshape(-1,3,32,32)
